4ce127d03aa35893188a62fe463454e0cf3f377d
hiro8watanabe <87076032+hiro8watanabe@users.noreply.github.com>
2022-05-28 09:23:31 +0900

    テスト完了

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # react_Rendering-optimization
+
 Created with CodeSandbox
+
+React による再レンダリング最適化のテストです。
--- src/components/ChildArea.jsx
+++ src/components/ChildArea.jsx
@@ -11,6 +11,7 @@ export const ChildArea = memo((props) => {
   const { open, onClickClose } = props;
   console.log("ChildAreaがレンダリング");
 
+  //わざと負荷をかける処理
   const data = [...Array(2000).keys()];
   data.forEach(() => {
     console.log("...");
